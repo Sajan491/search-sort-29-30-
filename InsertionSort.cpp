750c2bc0a248de33036ad99e8eaa40451753d6f6
sajan491 <sajanmahat491@gmail.com>
2020-02-20 10:33:17 +0545

    added header file

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,26 +1,8 @@
 #include<iostream>
+#include "sort.h"
 using namespace std;
 
-void InsertionSort(int arrayy[], int n){
-    int j, key, i=0, k=0;
-    for(j=1;j<n;j++){
-        key=arrayy[j];
-        i=j-1;
 
-        while(i>=0 && arrayy[i]>key){
-            arrayy[i+1]=arrayy[i];
-            i=i-1;
-
-        }
-        arrayy[i+1]=key;
-    }
-
-    for(k=0;k<n;k++){
-        cout<<arrayy[k]<<endl;
-    }
-
-
-}
 
 
 int main(){
