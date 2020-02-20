63ffe00979f1103cd2f3777e68adeacaea5b113a
Ishar Maharjan <isharmaharjan123@gmail.com>
2020-02-20 16:43:41 +0545

    implementation of sort func in binary search file

--- searching/BinarySearch.cpp
+++ searching/BinarySearch.cpp
@@ -1,3 +1,5 @@
+#include "../sorting/sort.h"
+
 #include<iostream>
 using namespace std;
 
@@ -5,27 +7,26 @@ int BinarySearch(int list[], int target, int size){
 	int min = 0;
 	int max = size - 1;
 	int mid;
-	while(min <= max){
+	while(max >= min){
 		mid = (min + max)/2;
 		if(list[mid] == target){
 			return mid;
 		}
 		else if(list[mid] < target){
-			min = mid;
-			mid = (min + mid)/2;
+			min = mid + 1;
 		}
 		else if(list[mid] > target){
-			max = mid;
-			mid = (min + mid)/2;
+			max = mid - 1;
 		}
 	}
 	return -1;
 }
 
 int main(){
-	int list[] = {1, 2, 3, 5, 4, 7, 10, 15, 20, 30, 32, 45, 87, 125, 265, 320, 420};
+	int list[] = {1, 2, 3, 5, 4, 7, 10, 15, 55, 200, 2, 33, 690, 20, 30, 32, 45, 87, 125, 265, 320, 420};
 	int index, size;
 	size = sizeof(list) / sizeof(list[0]);
-	index = BinarySearch(list, 265, size);
+	InsertionSort(list, size);
+	index = BinarySearch(list, 690, size);
 	cout<<index;
 }
